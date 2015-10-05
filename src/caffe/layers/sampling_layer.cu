8d7827f75fe3bf38482f1fa586374bec2af209e6
Xu Shen <shenxuustc@gmail.com>
2015-10-05 17:29:00 +0800

    finish the code of sampling layer

--- src/caffe/layers/sampling_layer.cu
+++ src/caffe/layers/sampling_layer.cu
@@ -14,7 +14,10 @@ namespace caffe{
 		const Dtype* sigma_data = bottom[1]->gpu_data();
 		Dtype* top_data = top[0]->mutable_gpu_data();
 		const int count = bottom[0]->count();
-		caffe_gpu_rng_gaussian(count, mu_data, sigma_data, top_data);
+		Dtype* gaussian_data = gaussian_value_.mutable_gpu_data();
+		caffe_gpu_rng_gaussian(count, Dtype(0.), Dtype(1.), gaussian_data);
+		caffe_gpu_mul(count, sigma_data, gaussian_data, top_data);
+		caffe_gpu_add(count, mu_data, top_data, top_data);
 	}
 
 	template <typename Dtype>
@@ -24,10 +27,14 @@ namespace caffe{
 		const int count = bottom[0]->count();
 		Dtype* mu_diff = bottom[0]->mutable_gpu_diff();
 		Dtype* sigma_diff = bottom[1]->mutable_gpu_diff();
-		const Dtype* mu_data = bottom[0]->gpu_data();
-		const Dtype* sigma_data = bottom[1]->gpu_data();
 		const Dtype* top_diff = top[0]->gpu_diff();
-		//TODO: add the gradient computation here
+		const Dtype* gaussian_data = gaussian_value_.gpu_data();
+		if (propagate_down[0]){
+			caffe_copy(count, top_diff, mu_diff);
+		}
+		if (propagate_down[1]){
+			caffe_gpu_mul(count, top_diff, gaussian_data, sigma_diff);
+		}
 	}
 
 	INSTANTIATE_LAYER_GPU_FUNCS(SamplingLayer);
